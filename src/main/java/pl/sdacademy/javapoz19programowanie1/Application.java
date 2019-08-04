ce6d9c96cee8c02488461737908e9069e3763114
warkoczek <53393030+warkoczek@users.noreply.github.com>
2019-08-04 10:01:07 +0200

    some change

--- src/main/java/pl/sdacademy/javapoz19programowanie1/Application.java
+++ src/main/java/pl/sdacademy/javapoz19programowanie1/Application.java
@@ -3,5 +3,6 @@ package pl.sdacademy.javapoz19programowanie1;
 public class Application {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("How are you");
     }
 }
